9780ecafe91d447651f1fcea109d05f4243bd39b
DimaPremiumFox <58880572+DimaPremiumFox@users.noreply.github.com>
2022-04-23 14:53:31 +0300

    Advanced Session Plugin

---
+++ Source/Ghostophobia/Ghostophobia.Build.cs
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class Ghostophobia : ModuleRules
+{
+	public Ghostophobia(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+	}
+}
